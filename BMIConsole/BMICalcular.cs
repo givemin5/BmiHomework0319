390fefe9a5b894855bc3aecb0651cf6426ab51a8
givemin5 <givemin5@gmail.com>
2017-03-19 22:30:32 +0800

    加入專案檔案。

---
+++ BMIConsole/BMICalcular.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BMIConsole
+{
+    public class BMICalcular
+    {
+        public BMICalcular()
+        {
+        }
+
+        internal string Calc(SexType sexType, decimal height, decimal weight)
+        {
+            //bmi = 體重 / (身高公尺)^2
+            decimal bmi = weight / ((height / 100m) * (height / 100m));
+
+            if (sexType == SexType.Man)
+                return ManBmiRsult(bmi);
+            else
+                return WomanBmiResult(bmi);
+        }
+
+        private string WomanBmiResult(decimal bmi)
+        {
+            if (bmi < 18)
+                return "太瘦";
+            else if (bmi > 22)
+                return "太胖";
+            return "適中";
+        }
+
+        private string ManBmiRsult(decimal bmi)
+        {
+            if (bmi < 20)
+                return "太瘦";
+            else if (bmi > 25)
+                return "太胖";
+            return "適中";
+        }
+    }
+}
---
+++ BMIConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMICalcular bmiCalc = new BMICalcular();
+
+            var height = 178m;
+            var weight = 81m;
+
+            string result = bmiCalc.Calc(SexType.Man,height, weight);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
